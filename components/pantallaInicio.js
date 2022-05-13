4fa3baa887b0f3c5f4f8711ce5a52e465e3ae55b
NADIA ARELLANO <nadia.arellano.g@gmail.com>
2022-05-13 11:44:10 -0400

    nosotros

--- components/pantallaInicio.js
+++ components/pantallaInicio.js
@@ -1,4 +1,15 @@
 let contadorClicks = 0;
+//agrega color a parrafo de "nosotros"
+function mouseOver() {
+  document.getElementsByClassName("somos").style.color = "red";
+}
+
+function mouseOut() {
+  document.getElementsByClassName("somos").style.color = "black";
+}
+
+console.log("probando")
+
 
 $(document).ready(function(){
   $('.card-img-top').click(function() {
