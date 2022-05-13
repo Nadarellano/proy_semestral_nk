7f66839ff6c2ab800aeba08d9e65ce6a58ea427e
NADIA ARELLANO <nadia.arellano.g@gmail.com>
2022-05-13 13:57:50 -0400

    o

--- components/pantallaInicio.js
+++ components/pantallaInicio.js
@@ -1,12 +1,5 @@
-let contadorClicks = 0;
 //agrega color a parrafo de "nosotros"
 
 console.log("probando")
 
 
-$(document).ready(function(){
-  $('.card-img-top').click(function() {
-    contadorClicks += 1;
-    console.log(contadorClicks);
-})
-});
