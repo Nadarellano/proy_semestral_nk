6e61a7ae3b6980a5d5d2a3ad0fe88c0b09b133d7
AKharhi <an.munoze@duocuc.cl>
2022-05-13 03:13:18 -0400

    validaciones

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     $("#form").validate({
         rules: {
-            name : {
+            name: {
                 required: true,
                 minlength: 10
             },
@@ -18,59 +18,61 @@ $(document).ready(function () {
                 min: 0,
                 minlength: 9
             },
-            city : {
+            city: {
                 required: true,
                 minlength: 3
-                
+
             },
 
-            txt_rut :{
-                required:true,
-               
+            txt_rut: {
+                required: true,
+
             },
-            message : {
+            message: {
                 required: true,
                 minlength: 30
             },
-            password: 
-            { required: true, 
-              minlength: 8,
-            formAlphanumeric: true},
+            password:
+            {
+                required: true,
+                minlength: 8,
+                formAlphanumeric: true
+            },
         },
 
 
 
-        messages : {
+        messages: {
             name: {
                 minlength: jQuery.validator.format("Por favor, al menos {0} caracteres son necesarios"),
-                required:"Este campo es requerido"
+                required: "Este campo es requerido"
             },
-            
+
             email: {
                 email: "El email debe tener el formato: abc@domain.tld",
-                required:"Este campo es requerido"
+                required: "Este campo es requerido"
             },
 
             numberPhone: {
-                number:"Por favor ingresa un número válido",
+                number: "Por favor ingresa un número válido",
                 minlength: jQuery.validator.format("Por favor, al menos {0} caracteres son necesarios"),
-                required:"Este campo es requerido"
+                required: "Este campo es requerido"
             },
             city: {
                 minlength: jQuery.validator.format("Por favor, al menos {0} caracteres son necesarios"),
-                required:"Este campo es requerido"
+                required: "Este campo es requerido"
             },
             txt_rut: {
                 // minlength: jQuery.validator.format("Por favor, al menos {0} caracteres son necesarios"),
-                required:"Este campo es requerido"
+                required: "Este campo es requerido"
             },
 
             message: {
                 minlength: jQuery.validator.format("Por favor, al menos {0} caracteres son necesarios"),
-                required:"Este campo es requerido"
+                required: "Este campo es requerido"
             },
-            password:{
-                required:"Este campo es requerido",
+            password: {
+                required: "Este campo es requerido",
                 minlength: jQuery.validator.format("Por favor, al menos {0} caracteres son necesarios"),
 
             },
@@ -79,51 +81,51 @@ $(document).ready(function () {
     });
 
     $.validator.addMethod("formAlphanumeric", function (value, element) {
-        var pattern = /^[\w]+$/i;
+        var pattern = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/; 
         return this.optional(element) || pattern.test(value);
-      }, "Se requiere que la contraseña sea alfanumérica (azAZ09)");
+    }, "La contraseña debe y tener al entre 8 y 16 caracteres y 1 letra mayúscula y 1 letra minúscula");
 
     var Fn = {
         // Valida el rut con su cadena completa "XXXXXXXX-X"
-        validaRut : function (rutCompleto) {
-          rutCompleto = rutCompleto.replace("‐","-");
-          if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test( rutCompleto ))
-            return false;
-          var tmp   = rutCompleto.split('-');
-          var digv  = tmp[1]; 
-          var rut   = tmp[0];
-          if ( digv == 'K' ) digv = 'k' ;
-          
-          return (Fn.dv(rut) == digv );
+        validaRut: function (rutCompleto) {
+            rutCompleto = rutCompleto.replace("‐", "-");
+            if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test(rutCompleto))
+                return false;
+            var tmp = rutCompleto.split('-');
+            var digv = tmp[1];
+            var rut = tmp[0];
+            if (digv == 'K') digv = 'k';
+
+            return (Fn.dv(rut) == digv);
         },
-        dv : function(T){
-          var M=0,S=1;
-          for(;T;T=Math.floor(T/10))
-            S=(S+T%10*(9-M++%6))%11;
-          return S?S-1:'k';
+        dv: function (T) {
+            var M = 0, S = 1;
+            for (; T; T = Math.floor(T / 10))
+                S = (S + T % 10 * (9 - M++ % 6)) % 11;
+            return S ? S - 1 : 'k';
         }
-      }
-      
-      
-      $("#btnvalida").click(function(){
-        if (Fn.validaRut( $("#txt_rut").val() )){
-          $("#msgerror").html(" ");
+    }
+
+
+    $("#btnvalida").click(function () {
+        if (Fn.validaRut($("#txt_rut").val())) {
+            $("#msgerror").html(" ");
         } else {
-          $("#msgerror").html("El Rut no es válido ");
+            $("#msgerror").html("El Rut no es válido ");
         }
-      });
- 
-      $("#msgerror").css("color", "#FF0000");
+    });
 
- 
+    $("#msgerror").css("color", "#FF0000");
 
 
-    
 
-    $('#form').submit(function(event) {
+
+
+
+    $('#form').submit(function (event) {
         console.log("Formulario enviado")
         event.preventDefault();
     });
-    
 
- });
+
+});
