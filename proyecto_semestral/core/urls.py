f949c26dac2967350d2a4074374dc9298f527276
Akharhi <an.munoze@duocuc.cl>
2022-06-05 20:53:30 -0400

    agregar bootstrap

--- proyecto_semestral/core/urls.py
+++ proyecto_semestral/core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, carrito, nosotros
+
 
 urlpatterns = [
     path('', index,name='index'),
+    path('carrito', carrito,name='carrito'),
+    path('nosotros', nosotros, name='nosotros')
+
+    
 ]
