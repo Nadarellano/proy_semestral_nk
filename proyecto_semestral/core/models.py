21a4dc6ae48374a29c9bcbc0eed4f90837d2c28b
Akharhi <an.munoze@duocuc.cl>
2022-06-07 02:23:03 -0400

    agregando nuevos atributos

--- proyecto_semestral/core/models.py
+++ proyecto_semestral/core/models.py
@@ -7,6 +7,8 @@ from django.db import models
 class Macetero(models.Model):
     idMacetero = models.IntegerField(primary_key=True, verbose_name="Id de macetero")
     colorMacetero = models.CharField(max_length= 50, verbose_name="Color de Macetero")
+    stockMacetero = models.IntegerField(default=0, verbose_name="Stock de Macetero")
+    precioMacetero = models.IntegerField(default=0, verbose_name="Precio de Macetero")
 
 def __str__(self):
     return self.colorMacetero
@@ -14,7 +16,9 @@ def __str__(self):
 
 class Sustrato(models.Model):
     idSustrato = models.IntegerField(primary_key=True, verbose_name="Id de sustrato")
-    tipoSustrato = models.CharField(max_length= 50, verbose_name="Tipo de Sustra")
+    tipoSustrato = models.CharField(max_length= 50, verbose_name="Tipo de Sustrato")
+    stockSustrato = models.IntegerField(default=0, verbose_name="Stock de Sustrato")
+    precioSustrato = models.IntegerField(default=0, verbose_name="Precio de Sustrato")
 
 def __str__(self):
     return self.tipoSustrato
@@ -24,6 +28,10 @@ def __str__(self):
 class Flor(models.Model):
     idFlor = models.IntegerField(primary_key=True, verbose_name="Id de flor")
     nombreFlor = models.CharField(max_length= 50, verbose_name="Nombre de Flor")
+    stockFlor = models.IntegerField(default=0, verbose_name="Stock de Flor")
+    precioFlor = models.IntegerField(default=0, verbose_name="Precio de Flor")
+
+
 
 def __str__(self):
     return self.tipoFlor
@@ -31,6 +39,8 @@ def __str__(self):
 class Arbusto(models.Model):
     idArbusto = models.IntegerField(primary_key=True, verbose_name="Id de Arbusto")
     nombreArbusto = models.CharField(max_length= 50, verbose_name="Nombre de Arbusto")
+    stockArbusto = models.IntegerField(default=0, verbose_name="Stock de Arbusto")
+    precioArbusto= models.IntegerField(default=0, verbose_name="Precio de Arbusto")
 
 def __str__(self):
     return self.nombreArbusto
@@ -44,6 +54,12 @@ class Cliente (models.Model):
     telefono = models.IntegerField(verbose_name="Telefono")
     ciudad = models.CharField(max_length = 100, verbose_name="Ciudad")
     comentario = models.TextField(verbose_name="Comentario")
+    direccion = models.CharField(max_length = 300, verbose_name="Dirección")
+    fechaRegistro = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de registro")
+    ultimaModificacion = models.DateTimeField(auto_now=True, verbose_name="Fecha de modificación")
+
+
+
 
 
     def __str__(self):
