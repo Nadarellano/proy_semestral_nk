efadd116e7e1e8a064770934f42c1521e097a017
Akharhi <an.munoze@duocuc.cl>
2022-06-10 03:56:05 -0400

    template contacto

--- proyecto_semestral/core/views.py
+++ proyecto_semestral/core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 # from requests import request
 from .models import Cliente, Arbusto, Macetero,Sustrato,Flor
-from .forms import ClienteForm
+from .forms import ClienteForm, ContactForm
+from django.urls import reverse
 
 
 # Create your views here.
@@ -251,6 +252,20 @@ def form_del_datos (request, id):
 
     return redirect(to="")
 
+
+def contact (request):
+    contact_form = ContactForm()
+
+    if request.method == 'POST':
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+           name = request.POST.get('name', '')
+           email = request.POST.get('email', '')
+           content = request.POST.get('content', '')
+           return redirect(reverse('contact')+"?ok")
+            
+
+    return render(request, 'core/contact.html',{'form':contact_form} )
 
 
 
