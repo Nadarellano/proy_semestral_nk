a4715f1efc119cb9ae847a156f48ea99c12e7614
Akharhi <an.munoze@duocuc.cl>
2022-06-24 19:44:59 -0400

    serializers all prod

--- proyecto_semestral/rest_producto/urls.py
+++ proyecto_semestral/rest_producto/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from rest_producto.views import lista_maceteros
+from rest_producto.views import lista_maceteros, lista_arbustos, lista_flores, lista_sustratos
 
 urlpatterns = [
-    path('lista_maceteros', lista_maceteros, name="lista_maceteros")
+    path('lista_maceteros', lista_maceteros, name="lista_maceteros"),
+    path('lista_arbustos', lista_arbustos, name="lista_arbustos"),
+    path('lista_flores', lista_flores, name="lista_flores"),
+    path('lista_sustratos', lista_sustratos, name="lista_sustratos")
 ]
--- proyecto_semestral/rest_producto/views.py
+++ proyecto_semestral/rest_producto/views.py
@@ -5,8 +5,8 @@ from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 
-from core.models import Macetero
-from .serializers import MacSerializer
+from core.models import Macetero, Sustrato, Arbusto, Flor
+from .serializers import MacSerializer, ArbustoSerializer, FlorSerializer, SustratoSerializer
 
 @csrf_exempt
 @api_view(['GET', 'POST'])
@@ -23,3 +23,54 @@ def lista_maceteros(request):
             return Response(mac_serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(mac_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+@api_view(['GET', 'POST'])
+def lista_arbustos(request):
+    if request.method == 'GET':
+        arbustos = Arbusto.objects.all()
+        arb_serializer = ArbustoSerializer(arbustos, many=True)
+        return Response(arb_serializer.data)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        arb_serializer = ArbustoSerializer(data=data)
+        if arb_serializer.is_valid():
+            arb_serializer.save()
+            return Response(arb_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(arb_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+@api_view(['GET', 'POST'])
+def lista_flores(request):
+    if request.method == 'GET':
+        flores = Flor.objects.all()
+        flor_serializer = FlorSerializer(flores, many=True)
+        return Response(flor_serializer.data)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        flor_serializer = FlorSerializer(data=data)
+        if flor_serializer.is_valid():
+            flor_serializer.save()
+            return Response(flor_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(flor_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+@api_view(['GET', 'POST'])
+def lista_sustratos(request):
+    if request.method == 'GET':
+        sustratos = Sustrato.objects.all()
+        sustrato_serializer = SustratoSerializer(sustratos, many=True)
+        return Response(sustrato_serializer.data)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        sustrato_serializer = SustratoSerializer(data=data)
+        if sustrato_serializer.is_valid():
+            sustrato_serializer.save()
+            return Response(sustrato_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(sustrato_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
