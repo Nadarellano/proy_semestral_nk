1bb67aa4096657ff9b077a1f14252bc9dfef21d9
Akharhi <an.munoze@duocuc.cl>
2022-06-24 23:11:29 -0400

    eliminar html

--- proyecto_semestral/rest_producto/serializers.py
+++ proyecto_semestral/rest_producto/serializers.py
@@ -6,23 +6,23 @@ from core.models import Arbusto, Flor, Sustrato, Macetero
 class MacSerializer(serializers.ModelSerializer):
     class Meta:
         model = Macetero
-        fields = ['idMacetero','colorMacetero','stockMacetero','precioMacetero','imagenMacetero']
+        fields = ['idMacetero','colorMacetero','stockMacetero','precioMacetero','imagenMacetero', 'detalleMacetero']
 
 
 class ArbustoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Arbusto
-        fields = ['idArbusto','nombreArbusto','stockArbusto','precioArbusto','imagenArbusto']
+        fields = ['idArbusto','nombreArbusto','stockArbusto','precioArbusto','imagenArbusto',  'detalleArbusto']
 
 class FlorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flor
-        fields = ['idFlor','nombreFlor','stockFlor','precioFlor','imagenFlor']
+        fields = ['idFlor','nombreFlor','stockFlor','precioFlor','imagenFlor',  'detalleFlor']
 
 class SustratoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sustrato
-        fields = ['idSustrato','tipoSustrato','stockSustrato','precioSustrato','imagenSustrato']
+        fields = ['idSustrato','tipoSustrato','stockSustrato','precioSustrato','imagenSustrato',  'detalleSustrato']
 
 
     
