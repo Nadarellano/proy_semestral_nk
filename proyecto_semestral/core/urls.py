afbcb382d55c22f86e9a2e7a3099c3c373c3eb37
NADIA ARELLANO <nadia.arellano.g@gmail.com>
2022-07-13 15:56:27 -0400

    crea buscar producto

--- proyecto_semestral/core/urls.py
+++ proyecto_semestral/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import form_mod_datos, index, carrito, nosotros, datos_seguimiento
 from .views import contact, form_del_direccion, form_direccion, stock, terminos_condiciones,  datos_despacho
 from .views import  pago, prod_arbustos, prod_flores, prod_maceteros,prod_sustratos, registrodos, form_direccion
-from .views import  ubicacion_compra, datos_cliente,  productos_recomendados, form_mod_datos, form_del_datos
+from .views import  ubicacion_compra, datos_cliente,  productos_recomendados, form_mod_datos, form_del_datos, buscar
 
 
 
@@ -30,5 +30,5 @@ urlpatterns = [
     path('datos_despacho', datos_despacho, name='datos_despacho'),
     path('form_direccion/<id>', form_direccion, name='form_direccion'),
     path('form_del_direccion/<id>', form_del_direccion, name='form_del_direccion'),
-    
+    path('buscar/', buscar),
 ]
